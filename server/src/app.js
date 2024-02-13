dce0af0af382445aeb74f76912bf4604ba14f8a3
denos <costelasdenis@gmail.com>
2024-02-13 16:20:50 +0200

    CORS Middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const planetsRouter = require('./routes/planets/planets.router')
 
@@ -6,5 +7,8 @@ const app = express();
 
 app.use(express.json())
 app.use(planetsRouter)
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 module.exports = app
