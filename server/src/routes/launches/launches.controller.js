36391688358afbc29dd213621852d0943bfdd2df
denos <costelasdenis@gmail.com>
2024-02-15 23:04:20 +0200

    post launch validation

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,4 @@
-const {getAllLaunches, addNewLaunch} = require('../../models/launches.model')
+const { getAllLaunches, addNewLaunch } = require('../../models/launches.model')
 
 function httpGetAllLaunches(req, res) {
     return res.status(200).json(getAllLaunches());
@@ -6,7 +6,19 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
     const launch = req.body;
+    if (!launch.mission || !launch.rocket || !launch.launchDate
+        || !launch.destination) {
+        return res.status(400).json({
+            error: 'Missing required launch property.'
+        });
+    }
+
     launch.launchDate = new Date(launch.launchDate);
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: 'Invalid Launch Date'
+        })
+    }
     addNewLaunch(launch)
 
     return res.status(201).json(launch)
