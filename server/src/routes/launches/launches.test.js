d3edb1f84a37624ae8173ffc062f6a10d8605ba4
denos <costelasdenis@gmail.com>
2024-02-18 19:17:55 +0200

    testing with jest

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,18 @@
+describe("Test GET /launches", () => {
+    test('200 Success', () => {
+        const response = 200;
+        expect(response).toBe(200);
+    })
+})
+
+describe("Test POST / launches", () => {
+    test('200 success', () => {
+
+    });
+    test('It should catch missing required properties', () => {
+
+    });
+    test('It should catch invalid dates', () => {
+
+    });
+})
